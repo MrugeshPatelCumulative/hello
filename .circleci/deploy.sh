4d613ffbcabb7fd70db07a894ccaef42e566867a
MrugeshPatelCumulative <83218048+MrugeshPatelCumulative@users.noreply.github.com>
2022-08-13 14:26:33 +0530

    add sh file

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cd /var/www/html/Mrugesh/
-touch test1.txt
-exit
+set -e
+
+# rm -rf .git
+# rm -rf .gitignore
+# git config --global user.email ""
+# git config --global user.name ""
+# mv .gitignore_dev .gitignore
+# git init .
+# git add .
+# git commit -m "Deploying"
+# git remote add production ssh://git@github.com:MrugeshPatelCumulative/hello.git
+# git push --force production master
+ssh $SSH_USER@$SSH_HOST "cd /var/www/html/Mrugesh/ && touch test1.txt && exit"
