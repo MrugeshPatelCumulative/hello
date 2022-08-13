99d9e670b02dbce7840175cb16fcb9ca8cb59213
MrugeshPatelCumulative <83218048+MrugeshPatelCumulative@users.noreply.github.com>
2022-08-13 15:00:45 +0530

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -13,8 +13,8 @@ set -e
 # git remote add production ssh://git@github.com:MrugeshPatelCumulative/hello.git
 # git push --force production master
 
-ssh $SSH_USER@$SSH_HOST
-
-echo "cd /var/www/html/Mrugesh/"
-echo "git pull https://github.com/MrugeshPatelCumulative/hello.git"
-echo "exit"
+ssh $SSH_USER@$SSH_HOST "
+    cd /var/www/html/Mrugesh/ &&
+    git pull https://github.com/MrugeshPatelCumulative/hello.git &&
+    exit
+"
